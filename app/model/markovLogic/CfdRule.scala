19d711e2aa554115688afcc23d2e4c05abeee3be
BenBrauer <benjamin.brauer@gmail.com>
2015-06-13 23:53:54 +0200

    First draft of Markov Logic Conversion for CFD-Rule implemented

--- app/model/markovLogic/CfdRule.scala
+++ app/model/markovLogic/CfdRule.scala
@@ -11,26 +11,28 @@ class CfdRule() {
     ""
   } 
   
-  private var _conditionalAttributeIdentifier = ""
+  var _conditionalAttributeIdentifier: Array[String] 
  
-  private var _consequentAttributeIdentifier = ""
+  var _consequentAttributeIdentifier: Array[String] 
   
   def setConditionalExpression(conditionalAttributeIdentifier: String, consequentAttributeIdentifier: String): String = {
-    _conditionalAttributeIdentifier = conditionalAttributeIdentifier
-    _consequentAttributeIdentifier = consequentAttributeIdentifier
+    var condAttId = conditionalAttributeIdentifier.replace("[", "").replace("]", "")
+    var consAttId = consequentAttributeIdentifier.replace("[", "").replace("]", "")
+    _conditionalAttributeIdentifier = condAttId.split(",")
+    _consequentAttributeIdentifier = consAttId.split(",")
     return ""
   }
   
   private var _tupleName = ""
   
-  private var _conditionalValueIdentifier = ""
+  var _conditionalValueIdentifier: Array[String] 
   
-  private var _consequentValueIdentifier = ""
+  var _consequentValueIdentifier: Array[String]
   
   def setTuple(tupleName: String, conditionalValueIdentifier: String, consequentValueIdentifier: String): String = {
     _tupleName = tupleName
-    _conditionalValueIdentifier = conditionalValueIdentifier
-    _consequentValueIdentifier = consequentValueIdentifier
+    _conditionalValueIdentifier = conditionalValueIdentifier.replace("[", "").replace("]", "").split(",")
+    _consequentValueIdentifier = consequentValueIdentifier.replace("[", "").replace("]", "").split(",")
     ""
   }
   //TODO:
@@ -39,6 +41,18 @@ class CfdRule() {
   override def toString = _relationName + "(" + _conditionalAttributeIdentifier + "->" + _consequentAttributeIdentifier +
                           "," + _tupleName + "=(" + _conditionalValueIdentifier + "||" + _consequentValueIdentifier + ")" 
   
+  def toMarkovLogic: Array[String] = {
+    val cons = _consequentAttributeIdentifier zip _consequentValueIdentifier
+    val cond = _conditionalAttributeIdentifier zip _conditionalValueIdentifier
+    val condPart = cond.foldLeft("")((condTotal, condTuple) => condTotal + { if (condTotal.length > 0)  "^" else "" } 
+      + condTuple._1 + "(id1," + { if(condTuple._2 == "_") "value"  else condTuple._2 } + ")^"
+      + condTuple._1  + "(id2," + {if(condTuple._2 == "_") "value" else condTuple._2} + ")")
+   for ((consAttr,consVal) <- cons) yield condPart + "->" + {if (consVal == "_") "SAME" + consAttr + "(id1,id2)" 
+     else consAttr + "(id1," + consVal + ")^" + consAttr + "(id2," + consVal  }
+  } 
+                              
+  
+                          
 }
 
 
