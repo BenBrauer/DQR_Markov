a07c9c9af14c456418f646f942cc00ad367e1e51
BenBrauer <benjamin.brauer@gmail.com>
2015-08-12 23:27:00 +0200

    * DatasetCompiler issue with match predicates solved * New Utility class Commons

--- app/model/markovLogic/DatasetCompiler.scala
+++ app/model/markovLogic/DatasetCompiler.scala
@@ -1,19 +1,25 @@
 package model.markovLogic
 
 import model.data.Dataset
+import play.api.Logger
+import util.{Commons => c}
 
+/**
+ * DatasetCompiler is a compiler to generate hidden markov logic predicates for a dataset
+ */
 object DatasetCompiler {
   def apply(set: Dataset): String = {
     //generate predicates for relation columns 
     val columnPredicates =  set.relations.foldLeft("")((totalLogic, rel) => 
-      totalLogic  + { if (totalLogic.length > 0) "\n"} +  rel.columns.foldLeft("")((totalColumns, column) => 
-        totalColumns + { if (totalColumns.length > 0) "\n"} + 
+      totalLogic  + c.addNewLine(totalLogic) +  rel.columns.foldLeft("")((totalColumns, column) => 
+        totalColumns + c.addNewLine(totalColumns) + 
            rel.label + "-" + column + "(" + rel.label + "id,val" + column + ")"
     ))
+    Logger.debug("\ncolumnPredicates: " + columnPredicates)
     //predicates for eq-Function
     val eqPredicates = set.relations.foldLeft("")((totalLogic, rel) => 
-      totalLogic  + { if (totalLogic.length > 0) "\n"} + rel.columns.foldLeft("")((totalColumns, column) => 
-        totalColumns + { if (totalColumns.length > 0) "\n" } + 
+      totalLogic  + c.addNewLine(totalLogic) + rel.columns.foldLeft("")((totalColumns, column) => 
+        totalColumns + c.addNewLine(totalColumns) + 
           "eq" + rel.label + "-" + column + "(" + rel.label + "id," + rel.label + "id)"
     ))
     //predicates for match-Function
@@ -22,21 +28,21 @@ object DatasetCompiler {
       rel.columns.foldLeft(listRel)((listRel,col) => listRel.::(rel.label, col)))
     //generate match-Predicates for all equal column-names
     var allColumnsTail = allColumns.tail
-    val matchPredicates = allColumns.foldLeft("")((ml,col)=>
+    val matchPredicates = allColumns.foldLeft("")((ml,col)=> ml + c.addNewLine(ml) +
       {
-       val colPredicates = allColumnsTail.foldLeft("")((mlcol,tailCol) => {
-         val (tailColName, tailColRel) = tailCol
-         val (colName, colRel) = col
+       val colPredicates = allColumnsTail.foldLeft("")((mlcol,tailCol) => mlcol + 
+       {
+         val (tailColRel, tailColName) = tailCol
+         val (colRel, colName) = col
          if (colName == tailColName && tailColRel != colRel)
-           "match" + colName + "(" + colRel + "id" + "," + tailColRel + "id)"
+           c.addNewLine(mlcol) + "match" + colName + "(" + colRel + "id" + "," + tailColRel + "id)"
          else
            ""
        })
        if (allColumnsTail.length > 0)
          allColumnsTail = allColumnsTail.tail
-       colPredicates 
+       colPredicates
       })
-   
     return columnPredicates + "\n" + eqPredicates + "\n" + matchPredicates
   }
 }
---
+++ app/util/Commons.scala
@@ -0,0 +1,7 @@
+package util
+
+object Commons {
+  
+  def addNewLine(s: String): String = {if(s.length() > 0) "\n" else ""}
+  
+}
