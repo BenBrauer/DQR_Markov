f431b5462cf0ac372cf19d5bbec5f463922d8419
BenBrauer <benjamin.brauer@gmail.com>
2015-08-19 23:42:22 +0200

    * code comments

--- app/controllers/Browser.scala
+++ app/controllers/Browser.scala
@@ -8,29 +8,52 @@ import model.data._
 import model.markovLogic.RelationCompiler
 
 /**
- * Browser is a controller for operations on Datasets like create, delete, show and on Relations like create, delete, show
+ * A controller for operations on Datasets and Relations like create, delete and show
  */
 object Browser extends Controller {
  
+  /**
+   * Stores all attributes of a Dataset
+   */
   case class DatasetData(label: String)  
+  
+  /**
+   * Form to map form input into DatasetData instance
+   */
   val datasetForm = Form(
     mapping(
         "label" -> text
     )(DatasetData.apply)(DatasetData.unapply) 
   )
   
+  /**
+   * Returns an Action which creates a dataset
+   */
   def createDataset() = Action { implicit request =>
     val datasetData = datasetForm.bindFromRequest.get
     Dataset.create(datasetData.label)
     Redirect("/browser")
   }
   
+  /**
+   * Returns an Action which deletes a Dataset
+   * 
+   * @param id the Dataset's id
+   */
   def deleteDataset(id: Long) = Action {
     Dataset.delete(id)
     Redirect("/browser")
   }
  
+  /**
+   * Stores all attributes of a Relation
+   */
   case class RelationData(label: String, data: String, dataset_id: Long)
+  
+  
+  /**
+   * Form to map form input into RelationData instance
+   */ 
   val relationForm = Form(
       mapping(
         "label" -> text,
@@ -39,26 +62,48 @@ object Browser extends Controller {
           )(RelationData.apply)(RelationData.unapply)
           
     )
-     
+  
+  /**
+   * Returns an Action which starts the input of a Relation
+   * 
+   * @param dataset_id the id of the Dataset the Relation belongs to
+   */
   def inputRelation(dataset_id: Long) = Action {
 	  Ok(views.html.relationForm(relationForm,dataset_id))
   }
     
-   def createRelation() = Action { implicit request => 
+  /**
+   * Returns an Action which creates a Relation on the database
+   * 
+   */
+  def createRelation() = Action { implicit request => 
     val relationData = relationForm.bindFromRequest.get
     Relation.create(relationData.label, relationData.data, relationData.dataset_id)
     Redirect("/browser")
   }
   
+  /**
+   * Returns an Action which starts the view to show a relation
+   */
   def showRelation(id: Long) = Action {
     Ok(views.html.relation(Relation.byId(id)))
   }
    
+  /**
+   * Returns an Action which deletes a Relation
+   * 
+   * @param id the id of the Relation to be deleted
+   */
   def deleteRelation(id: Long) = Action {
     Relation.delete(id)
     Redirect("/browser")
   }
   
+  /**
+   * Returns an Action which outputs Markov Logic of a Relation
+   * 
+   * @param id the id of the relation
+   */
   def markovLogicRelation(id: Long) = Action {
     val relation = Relation.byId(id)
     Ok(RelationCompiler(relation))
--- app/controllers/Rules.scala
+++ app/controllers/Rules.scala
@@ -14,6 +14,9 @@ import play.api.data.validation._
  */
 object Rules extends Controller {
   
+  /**
+   * Stores all attributes of a Rule
+   */
   case class RuleData(id: Long, label: String, rule: String, dataset_id: Long) {
     //TODO: validate to all rule types 
     /*val ruleValidationConstraint = Constraint[String] {(r: String) => r match {
@@ -22,94 +25,139 @@ object Rules extends Controller {
     }*/
      }
     
-  
-    val ruleForm = Form(
+  /**
+   * Form to mmap from input into RuleData instance
+   */
+  val ruleForm = Form(
       mapping(
         "id" -> longNumber,
         "label" -> text(minLength = 0, maxLength = 255),
         "rule" -> text(minLength = 0, maxLength = 500),//.verifying(ruleValidationConstraint),
         "dataset_id" -> longNumber
           )(RuleData.apply)(RuleData.unapply)
-      )
+  )
     
-
-    def inputRule(dataset_id: Long) = Action {
-      val newRuleForm =ruleForm.fill(new RuleData(0, "","",dataset_id))
-      val saveAction:Call = routes.Rules.createRule(dataset_id)
-      Ok(views.html.ruleForm(newRuleForm,"Create new rule", saveAction, dataset_id))
-    }
-    
-    def createRule(dataset_id: Long) = Action { implicit request =>
-      ruleForm.bindFromRequest.fold(
-          formWithErrors => {
-            val saveAction:Call = routes.Rules.createRule(dataset_id)
-            BadRequest(views.html.ruleForm(formWithErrors,"Create new rule (Errors)", saveAction, dataset_id))
-          },
-          ruleData => {
-            Rule.create(ruleData.label, ruleData.rule, ruleData.dataset_id)
-            Redirect(routes.Rules.rules(dataset_id))
-          })
-    }
+  /**
+   * Returns an Action which starts the input of a Rule
+   * 
+   * @param dataset_id id of the rule's dataset
+   */
+  def inputRule(dataset_id: Long) = Action {
+    val newRuleForm =ruleForm.fill(new RuleData(0, "","",dataset_id))
+    val saveAction:Call = routes.Rules.createRule(dataset_id)
+    Ok(views.html.ruleForm(newRuleForm,"Create new rule", saveAction, dataset_id))
+  }
   
-    def editRule(id: Long) = Action {
-      val rule = Rule.byId(id)
-      if (rule != null) {
-        val filledRuleForm = ruleForm.fill(new RuleData(id, rule.label, rule.rule, rule.dataset_id))
-        val saveAction:Call = routes.Rules.updateRule
-        Ok(views.html.ruleForm(filledRuleForm,"Edit rule " + rule.label , saveAction, rule.dataset_id))
-      } else {
-        NotFound("No Rule found")
-      }
-    }
+  /**
+   * Returns an Action which creates a rule 
+   * 
+   * @param dataset_id id of the dataset the rule belongs to
+   */
+  def createRule(dataset_id: Long) = Action { implicit request =>
+    ruleForm.bindFromRequest.fold(
+        formWithErrors => {
+          val saveAction:Call = routes.Rules.createRule(dataset_id)
+          BadRequest(views.html.ruleForm(formWithErrors,"Create new rule (Errors)", saveAction, dataset_id))
+        },
+        ruleData => {
+          Rule.create(ruleData.label, ruleData.rule, ruleData.dataset_id)
+          Redirect(routes.Rules.rules(dataset_id))
+        })
+  }
   
-    def updateRule() = Action { implicit request =>
+  /**
+   * Returns an Action which starts the editable form of a Rule
+   * 
+   * @param id id of the Rule to be edited
+   */
+  def editRule(id: Long) = Action {
+    val rule = Rule.byId(id)
+    if (rule != null) {
+      val filledRuleForm = ruleForm.fill(new RuleData(id, rule.label, rule.rule, rule.dataset_id))
+      val saveAction:Call = routes.Rules.updateRule
+      Ok(views.html.ruleForm(filledRuleForm,"Edit rule " + rule.label , saveAction, rule.dataset_id))
+    } else {
+      NotFound("No Rule found")
+    }
+  }
+  
+  /**
+   * Returns an Action which updates a Rule
+   * 
+   */
+  def updateRule() = Action { implicit request =>
       val ruleData = ruleForm.bindFromRequest.get
       Rule.update(ruleData.id, ruleData.label, ruleData.rule)
       Ok(views.html.rules(Dataset.byId(ruleData.dataset_id)))
     }
   
-    def deleteRule(id: Long) = Action {
-      val rule = Rule.byId(id)
-      Rule.delete(id)
-      Redirect(routes.Rules.rules(rule.dataset_id))
-    }
+  /**
+   * Returns an Action which deletes a Rule
+   * 
+   * @param id of the Rule to deleted
+   */
+  def deleteRule(id: Long) = Action {
+    val rule = Rule.byId(id)
+    Rule.delete(id)
+    Redirect(routes.Rules.rules(rule.dataset_id))
+  }
+
+  /**
+   * Returns an Action which shows all rules belonging to the Dataset
+   * 
+   * @param dataset_id id of the dataset 
+   */
+  def rules(dataset_id: Long) = Action {
+    Ok(views.html.rules(Dataset.byId(dataset_id)))
+  }
   
-    def rules(dataset_id: Long) = Action {
-      Ok(views.html.rules(Dataset.byId(dataset_id)))
+  /**
+   * Returns an Action which compiles a rule into Markov Logic
+   * 
+   * @param id id of the rule to be compiled
+   */
+  def markovLogicRule(id: Long) = Action {
+    val rule = Rule.byId(id)
+    if (rule != null) {
+      Ok(RuleCompiler(rule))
+    } else{
+      NotFound("Rule does not exist")
     }
-    
-    def markovLogicRule(id: Long) = Action {
-      val rule = Rule.byId(id)
-      if (rule != null) {
-        Ok(RuleCompiler(rule))
-      } else{
-        NotFound("Rule does not exist")
-      }
-    }
-    
-    def mlFile(id: Long) = Action {
-      val dataset = Dataset.byId(id)
-      if (dataset != null) {
-        val ml = DatasetCompiler(dataset) + "\n\n" + 
-           dataset.rules.foldLeft("")(_ + "\n" +  RuleCompiler(_))
-        Ok(ml).as("application/x-download").withHeaders(
-          ("Content-disposition","attachment; filename=" + dataset.label + ".ml")
-        ) 
-      } else {
-        NotFound("Dataset does not exist")
-      }
-    }
-    
-    def dbFile(id: Long) = Action {
-      val dataset = Dataset.byId(id)
-      if (dataset != null) {
-        val db = dataset.relations().foldLeft("")(_ + "\n" + RelationCompiler(_))
-        Ok(db).as("application/x-download").withHeaders(
-          ("Content-disposition","attachment; filename=" + dataset.label + ".db")
-        )
-      } else {
-        NotFound("Dataset does not exist")
-      }
-    }
+  }
   
+   /**
+    * Returns an Action which creates an .ml-File from the Dataset
+    * 
+    * @param id id of the Dataset of which .ml-File should be created
+    */
+  def mlFile(id: Long) = Action {
+    val dataset = Dataset.byId(id)
+    if (dataset != null) {
+      val ml = DatasetCompiler(dataset) + "\n\n" + 
+         dataset.rules.foldLeft("")(_ + "\n" +  RuleCompiler(_))
+      Ok(ml).as("application/x-download").withHeaders(
+        ("Content-disposition","attachment; filename=" + dataset.label + ".ml")
+      ) 
+    } else {
+      NotFound("Dataset does not exist")
+    }
+  }
+  
+  /**
+    * Returns an Action which creates a .db-File from the Dataset
+    * 
+    * @param id id of the Dataset of which .db-File should be created
+    */
+  def dbFile(id: Long) = Action {
+    val dataset = Dataset.byId(id)
+    if (dataset != null) {
+      val db = dataset.relations().foldLeft("")(_ + "\n" + RelationCompiler(_))
+      Ok(db).as("application/x-download").withHeaders(
+        ("Content-disposition","attachment; filename=" + dataset.label + ".db")
+      )
+    } else {
+      NotFound("Dataset does not exist")
+    }
+  }
+
 }
